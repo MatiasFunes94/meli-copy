cb7f2f2292c4755fc145d7048409bc1601aecbfd
Matias Funes <matiasfunes94@gmail.com>
2020-10-20 16:42:36 -0300

    Updates

---
+++ precache-manifest.782b360ab53bff8d5342bb8e2cd728c2.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f5f3e6bafac495ce29ac3325a793da",
+    "url": "/meli-copy/index.html"
+  },
+  {
+    "revision": "908084bc2d97a64ab291",
+    "url": "/meli-copy/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f0af7e05da0f1aa81187",
+    "url": "/meli-copy/static/css/main.79d08502.chunk.css"
+  },
+  {
+    "revision": "908084bc2d97a64ab291",
+    "url": "/meli-copy/static/js/2.2c842264.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/meli-copy/static/js/2.2c842264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0af7e05da0f1aa81187",
+    "url": "/meli-copy/static/js/main.96f41da1.chunk.js"
+  },
+  {
+    "revision": "6fa259cb3bb2e92e39fe",
+    "url": "/meli-copy/static/js/runtime-main.5ac8034c.js"
+  },
+  {
+    "revision": "0bc95c8434adacfdf7a2b9934c022067",
+    "url": "/meli-copy/static/media/bg.0bc95c84.jpg"
+  },
+  {
+    "revision": "5174c4a3670d3f1b41ff69d2877a50d2",
+    "url": "/meli-copy/static/media/cano.5174c4a3.webp"
+  },
+  {
+    "revision": "5babdd57fdca5bf94d3d159e2d508961",
+    "url": "/meli-copy/static/media/image1.5babdd57.webp"
+  },
+  {
+    "revision": "1df8c78b86a61c6f030d663979fffa02",
+    "url": "/meli-copy/static/media/image2.1df8c78b.webp"
+  },
+  {
+    "revision": "fd72c029794f9f802e27d96d237bb7f5",
+    "url": "/meli-copy/static/media/image3.fd72c029.webp"
+  },
+  {
+    "revision": "6fd33a993b62892a698a3387be723b69",
+    "url": "/meli-copy/static/media/image4.6fd33a99.webp"
+  },
+  {
+    "revision": "1ef80ff2520d521a5a3c6f980f253984",
+    "url": "/meli-copy/static/media/image5.1ef80ff2.webp"
+  },
+  {
+    "revision": "29ce04ff993a18faba4b7b5b56ec25b9",
+    "url": "/meli-copy/static/media/imagen1.29ce04ff.jpeg"
+  },
+  {
+    "revision": "9bdd02cc0ee688b8d2de926f2795a05d",
+    "url": "/meli-copy/static/media/imagen1.9bdd02cc.webp"
+  },
+  {
+    "revision": "829c3706f35917e9c4928228f445c12a",
+    "url": "/meli-copy/static/media/imagen2.829c3706.webp"
+  },
+  {
+    "revision": "1792aa78802b236b519060b4427db88e",
+    "url": "/meli-copy/static/media/landing-ecommerce.1792aa78.png"
+  },
+  {
+    "revision": "92d7503503ea1549098bd275b5431535",
+    "url": "/meli-copy/static/media/meli.92d75035.jpeg"
+  }
+]);
